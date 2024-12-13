2acfa3dc08db1e9dbf1075b60cfab862d1609ac5
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2024-12-13 13:42:45 +0000

    user controller test passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      render 'users/create'
+    end
+  end
+
+  private
+
+  def user_params
+    params.required(:user).permit(:username, :email, :password)
+  end
 end
