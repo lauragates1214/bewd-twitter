7a2f509211eec2eeff4bcc3c492830da0b9fab15
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2024-12-16 11:25:31 +0000

    all but final tweets controller tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   delete '/sessions' => 'sessions#destroy'
 
   # TWEETS
+  post '/tweets' => 'tweets#create'
+  delete '/tweets/:id' => 'tweets#destroy'
+  get '/tweets' => 'tweets#index'
+  get '/users/:username/tweets' => 'tweets#index_by_user'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
